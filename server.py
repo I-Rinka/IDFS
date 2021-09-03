56053033f3c28210f53ef096ea491612803194ee
I-Rinka <I_Rinka@outlook.com>
2021-09-03 20:15:37 +0800

    ok

--- server.py
+++ server.py
@@ -1,3 +1,5 @@
 import net.listener as server
+import util.tools as ut
+print("My name: %s\nMy id: %s\n" % (ut.GetMyDevName(), ut.GetMyDeviceID()))
 server.db.create_table()
 server.LOCAL_SERVER.serve_forever()
